0795e67f2b316b5ef08c4b145cf41830da34d90c
Keyur Shah <keyurshah3939@gmail.com>
2023-02-05 05:31:23 +0530

    Testing Bed Is Prepared

--- libs/rate-tables/src/lib/rate-tables-2/rate-tables-2.component.ts
+++ libs/rate-tables/src/lib/rate-tables-2/rate-tables-2.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'rps-bull-rate-tables-2',
   standalone: true,
   imports: [],
+  encapsulation:ViewEncapsulation.ShadowDom,
   templateUrl: './rate-tables-2.component.html',
   styleUrls: ['./rate-tables-2.component.scss'],
 })
