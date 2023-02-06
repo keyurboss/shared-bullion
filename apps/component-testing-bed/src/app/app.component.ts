77870a4b9b10545274ea37ddc6cf7beb95e96ba7
sahilpatel <19dce113@charusat.edu.in>
2023-02-06 14:48:25 +0530

    rate table coponents ready

--- apps/component-testing-bed/src/app/app.component.ts
+++ apps/component-testing-bed/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RateTables2Component } from '@rps/bullion-rate-tables';
 import { RateTables1Component } from '@rps/bullion-rate-tables/table1';
 @Component({
   standalone: true,
-  imports: [RateTables1Component],
+  imports: [RateTables1Component ,RateTables2Component],
   selector: 'shared-bullion-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
