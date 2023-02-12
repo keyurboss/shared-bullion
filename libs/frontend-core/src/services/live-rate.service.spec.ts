75055a7bf4492e154f76027c10063a8c2aee54ab
Keyur Shah <keyurshah3939@gmail.com>
2023-02-13 03:07:33 +0530

    TESTS Pending

--- libs/frontend-core/src/services/live-rate.service.spec.ts
+++ libs/frontend-core/src/services/live-rate.service.spec.ts
@@ -1,13 +1,33 @@
-import { TestBed } from '@angular/core/testing';
-
+import {
+  BaseSymbolePriceInterface,
+  RateBaseSymboles,
+  RateBaseSymbolesArray,
+} from '@rps/bullion-interfaces';
 import { LiveRateService } from './live-rate.service';
+import faker from '@faker-js/faker';
+class LiveRateServiceTests extends LiveRateService {
+  async getLastRates(): Promise<
+    Record<RateBaseSymboles, BaseSymbolePriceInterface>
+  > {
+    return {
+      GOLD: {
+        
+      },
+      GOLD_MCX: {},
+      GOLD_SPOT: {},
+      INR: {},
+      SILVER: {},
+      SILVER_MCX: {},
+      SILVER_SPOT: {},
+    };
+  }
+}
 
-describe('DemoService', () => {
+describe('ABS LiveRateService', () => {
   let service: LiveRateService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(LiveRateService);
+    // service=new LiveRateService()
   });
 
   it('should be created', () => {
