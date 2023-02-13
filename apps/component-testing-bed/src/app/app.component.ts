ddb5df8fca42655976f066d278c75c5f4ad1ab98
Keyur Shah <keyurshah3939@gmail.com>
2023-02-13 12:49:13 +0530

    Provided Live Service

--- apps/component-testing-bed/src/app/app.component.ts
+++ apps/component-testing-bed/src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { RateTables1Component } from '@rps/bullion-rate-tables/table1';
+import {
+  DemoLiveRateService,
+  LiveRateService,
+} from '@rps/buillion-frontend-core/services';
 @Component({
   standalone: true,
   imports: [RateTables1Component],
   selector: 'shared-bullion-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  providers: [
+    {
+      provide: LiveRateService,
+      useClass: DemoLiveRateService,
+    },
+  ],
 })
 export class AppComponent {
   title = 'component-testing-bed';
