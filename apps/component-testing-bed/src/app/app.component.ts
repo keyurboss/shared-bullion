4c5a26c91b2a94bc6fd264ad5afc2e158de8f560
Sahil <sahilpatel3584@gmail.com>
2023-02-14 18:24:40 +0530

    rate-table-2

--- apps/component-testing-bed/src/app/app.component.ts
+++ apps/component-testing-bed/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RateTables1Component } from '@rps/bullion-rate-tables';
 import { RateTables2Component } from '@rps/bullion-rate-tables';
-import { RateTables3Component } from '@rps/bullion-rate-tables';
 import { LiveRateService } from '@rps/buillion-frontend-core/services';
 import { DemoLiveRateService } from '@rps/buillion-frontend-core/mock';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -9,7 +8,7 @@ import { observable } from 'rxjs';
 import { Env } from '@rps/buillion-frontend-core';
 @Component({
   standalone: true,
-  imports: [RateTables1Component ,RateTables2Component,ReactiveFormsModule,RateTables3Component],
+  imports: [RateTables1Component ,RateTables2Component,ReactiveFormsModule],
   selector: 'shared-bullion-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
