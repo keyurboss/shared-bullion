7e68e8ee8291f12217fa7098dad679abb6c968cf
PrathamPatel232 <patelpratham232@gmail.com>
2023-02-17 23:28:55 +0530

    rate-table-4 done

--- apps/component-testing-bed/src/app/app.component.ts
+++ apps/component-testing-bed/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RateTables1Component } from '@rps/bullion-rate-tables/table1';
+import { RateTables4Component } from '@rps/bullion-rate-tables/table4';
+
 import { LiveRateService } from '@rps/buillion-frontend-core/services';
 import { DemoLiveRateService } from '@rps/buillion-frontend-core/mock';
+import { Env } from '@rps/buillion-frontend-core/core';
+import { observable } from 'rxjs';
 @Component({
   standalone: true,
-  imports: [RateTables1Component],
+  imports: [RateTables1Component, RateTables4Component],
   selector: 'shared-bullion-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
@@ -12,6 +16,10 @@ import { DemoLiveRateService } from '@rps/buillion-frontend-core/mock';
     {
       provide: LiveRateService,
       useClass: DemoLiveRateService,
+    },
+    {
+      provide: Env,
+      useValue: {},
     },
   ],
 })
