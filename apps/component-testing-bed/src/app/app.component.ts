7390a7de704b0158576334cd175e04cc5dd55e98
AlayAp <alayap510@gmail.com>
2023-02-18 03:31:50 -0500

    table 1 done containig 2 columes

--- apps/component-testing-bed/src/app/app.component.ts
+++ apps/component-testing-bed/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RateTables1Component } from '@rps/bullion-rate-tables/table1';
-import { LiveRateService } from '@rps/buillion-frontend-core/services';
 import { DemoLiveRateService } from '@rps/buillion-frontend-core/mock';
+import { LiveRateService } from '@rps/buillion-frontend-core/services';
+import { RateTables7Component } from '@rps/bullion-rate-tables';
 @Component({
   standalone: true,
-  imports: [RateTables1Component],
+  imports: [RateTables7Component],
   selector: 'shared-bullion-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
