a0246818446e0fd7f94d2670fbe7f1f2642cfdc5
Keyur Shah <keyurshah3939@gmail.com>
2023-03-14 17:28:51 +0530

    NX Version Bumped

--- libs/core/validator-roots/jest.config.ts
+++ libs/core/validator-roots/jest.config.ts
@@ -2,14 +2,15 @@
 export default {
   displayName: 'core-validator-roots',
   preset: '../../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  globals: {},
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../coverage/libs/core/validator-roots',
