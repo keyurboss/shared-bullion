e059fb713a525f7d4deb3d6fabcb0863a0fa5426
sahiiilpatel <106461848+sahiiilpatel@users.noreply.github.com>
2023-03-16 15:40:50 +0530

    feat(rate-tables): Rate Table 6 (#17)
    Co-authored-by: Keyur Shah <keyurshah3939@gmail.com>

--- apps/component-testing-bed/src/app/app.component.ts
+++ apps/component-testing-bed/src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { Env } from '@rps/buillion-frontend-core';
 import { DemoLiveRateService } from '@rps/buillion-frontend-core/mock';
 import { LiveRateService } from '@rps/buillion-frontend-core/services';
-import {
-  RateTables1Component,
-  RateTables3Component,
-} from '@rps/bullion-rate-tables';
+import { RateTables1Component } from '@rps/bullion-rate-tables/table1';
+import { RateTables6Component } from '@rps/bullion-rate-tables/table6';
+
 @Component({
   standalone: true,
-  imports: [RateTables1Component, RateTables3Component],
+  imports: [RateTables1Component,RateTables6Component],
   selector: 'shared-bullion-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
