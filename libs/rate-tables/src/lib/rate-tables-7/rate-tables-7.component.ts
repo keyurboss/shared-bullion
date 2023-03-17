d71acdc5df9f7a7ac2c6b9e511cbd4baead16ed3
alayAp <alayap510@gmail.com>
2023-03-17 10:43:24 +0530

    component table 7 update and live rate were added

--- libs/rate-tables/src/lib/rate-tables-7/rate-tables-7.component.ts
+++ libs/rate-tables/src/lib/rate-tables-7/rate-tables-7.component.ts
@@ -1,11 +1,37 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-
+import { Component, ViewEncapsulation, Inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import {
+  LiveRateService,
+  RateObserDataType,
+} from '@rps/buillion-frontend-core';
+import { AsyncPipe, JsonPipe, NgFor, NgIf } from '@angular/common';
 @Component({
   selector: 'rps-bull-rate-tables-7',
   standalone: true,
-  imports: [],
-  encapsulation:ViewEncapsulation.ShadowDom,
+  imports: [AsyncPipe, JsonPipe, NgFor, NgIf],
+  encapsulation: ViewEncapsulation.ShadowDom,
   templateUrl: './rate-tables-7.component.html',
   styleUrls: ['./rate-tables-7.component.scss'],
 })
-export class RateTables7Component {}
+export class RateTables7Component {
+  GOLD: Observable<RateObserDataType>;
+  SILVER: Observable<RateObserDataType>;
+
+  constructor(@Inject(LiveRateService) alay: LiveRateService) {
+    this.GOLD = alay.RateObser$.GOLD_SPOT.asObservable();
+    this.SILVER = alay.RateObser$.SILVER_SPOT.asObservable();
+  }
+  silver_data=[
+      {
+        name: "SILVER. BAR 99 TCS",
+      },
+      {
+        name: "Silver Peti RJT TCS",
+      },
+      {
+        name: "Silver Bar RR TCS",
+      },
+    ]
+ 
+
+  }
