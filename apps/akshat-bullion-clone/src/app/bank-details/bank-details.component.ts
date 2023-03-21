e44ad84ad33b99562c2524d540eabd7eb94eeb06
Sahil <sahilpatel3584@gmail.com>
2023-03-21 16:37:57 +0530

    work on about-us && bank-details components

--- apps/akshat-bullion-clone/src/app/bank-details/bank-details.component.ts
+++ apps/akshat-bullion-clone/src/app/bank-details/bank-details.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgFor} from '@angular/common';
+// interface bankdata{
+//   tittle:string;
+//   data:string;
+// }
 @Component({
   selector: 'akshat-bull-app-bank-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,NgFor],
   templateUrl: './bank-details.component.html',
   styleUrls: ['./bank-details.component.scss'],
 })
-export class BankdetailsComponent {}
+export class BankdetailsComponent {
+// data:bankdata[]=[
+//   {
+//     tittle:'Account Name',
+//     data:'Akshat Bullion',
+//   },
+//   {
+//     tittle:'Bank Name',
+//     data:'Axis Bank',
+//   },
+//   {
+//     tittle:'Account No',
+//     data:'9200-2006-575-5340',
+//   },
+//   {
+//     tittle:'IFSC Code',
+//     data:'UTIB-000-4758',
+//   },
+//   {
+//     tittle:'Branch Name',
+//     data:'Balasinor',
+//   },
+// ];
+}
