1897276f741c9c964253749ca4aa958c90cbadc9
Sahil <sahilpatel3584@gmail.com>
2023-03-23 18:19:01 +0530

    footer component 50% completed

--- apps/akshat-bullion-clone/src/app/footer/footer.component.ts
+++ apps/akshat-bullion-clone/src/app/footer/footer.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-interface navbar{
-  name:string;
-  routerLink:string;
-}
+
 @Component({
   selector: 'akshat-bull-app-footer',
   standalone: true,
@@ -13,22 +10,15 @@ interface navbar{
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-  data: navbar[]=[
-    {
-      name:'ABOUT US',
-      routerLink:'/about-us',
-    },
-    {
-      name:'MESSAGES',
-      routerLink:'/messages',
-    },
-    {
-      name:'BANK DETAILS',
-      routerLink:'/bank-details',
-    },
-    {
-      name:'CONTACT US',
-      routerLink:'/contact-us',
-    },
-  ];
+footer_data={
+  aboutus:'About Us ',
+  aboutus_r_l:'/about-us',
+  message:'Messages',
+  message_r_l:'/messages',
+  bank_d:'Bank Details',
+  bank_d_r_l:'/bank-details',
+  contact_us:'Contact Us',
+  contact_us_r_l:'/contact-us',
+  copyright_text:'Akshat Bullion © 2021 I All Rights Reserved',
+};
 }
