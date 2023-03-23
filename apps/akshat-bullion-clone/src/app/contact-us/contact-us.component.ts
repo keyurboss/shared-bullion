344f06ad9a4ee91d951ef1e8df56afeac0624dbe
Sahil <sahilpatel3584@gmail.com>
2023-03-23 11:48:02 +0530

    contact-us component completed

--- apps/akshat-bullion-clone/src/app/contact-us/contact-us.component.ts
+++ apps/akshat-bullion-clone/src/app/contact-us/contact-us.component.ts
@@ -6,8 +6,26 @@ import { HeaderComponent } from '../header/header.component';
 @Component({
   selector: 'akshat-bull-app-contact-us',
   standalone: true,
-  imports: [CommonModule,HeaderComponent,RouterOutlet],
+  imports: [CommonModule, HeaderComponent, RouterOutlet],
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.scss'],
 })
-export class ContactUsComponent {}
+export class ContactUsComponent {
+  Address = {
+    tittle: 'OFFICE ADDRESS',
+    line1: 'Nr Pipala Sheri',
+    line2: 'Main Bazar',
+    line3: 'Balasinor, Mahisagar',
+    line4: 'Gujarat,388255',
+  };
+  Contact = {
+    tittle: 'Contact Numbers',
+    line1: 'Personal',
+    line2: 'Akshat Shah',
+    line3: '+91 8320361328',
+  };
+  Email={
+    tittle: 'Email',
+    line1:'akshatshah185@gmail.com',
+  };
+}
