4f10a37d19f1fa66d9508b14b0e345fdae1b8d96
PrathamPatel232 <patelpratham232@gmail.com>
2023-03-24 12:10:18 +0530

    app componant done

--- apps/akshat-gold-clone/src/app/app.routes.ts
+++ apps/akshat-gold-clone/src/app/app.routes.ts
@@ -3,65 +3,72 @@ import { HomeComponent } from './home/home.component';
 // import * as path from 'path';
 
 export const appRoutes: Route[] = [
-    {
-      path: '',
-      component: HomeComponent,
-      children: [
-        {
-          path: '',
-          redirectTo: 'live-rate',
-          pathMatch: 'full',
-        },
-        {
-          path: 'about-us',
-          loadComponent: () =>
-            import('./about-us/about-us.component').then((a) => a.AboutUsComponent),
-        },
-        {
-          path: 'live-rate',
-          loadComponent: () =>
-            import('./live-rate/live-rate.component').then(
-              (a) => a.LiveRateComponent
-            ),
-        },
-        {
-          path: 'updates',
-          loadComponent: () =>
-            import('./updates/updates.component').then((a) => a.UpdatesComponent),
-        },
-        {
-          path: 'bank-details',
-          loadComponent: () =>
-            import('./bank-details/bank-details.component').then(
-              (a) => a.BankDetailsComponent
-            ),
-        },
-        {
-          path: 'eco-calender',
-          loadComponent: () =>
-            import('./eco-calendar/eco-calendar.component').then(
-              (a) => a.EcoCalendarComponent
-            ),
-        },
-        {
-          path: 'contact-us',
-          loadComponent: () =>
-            import('./contact-us/contact-us.component').then(
-              (a) => a.ContactUsComponent
-            ),
-        },
-        {
-          path: 'login',
-          loadComponent: () =>
-            import('./login/login.component').then(
-              (a) => a.LoginComponent
-            ),
-        },
-        {
-          path: '**',
-          redirectTo: 'live-rate',
-          pathMatch: 'full',
-        },
-      ],
-    },
-  ];
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'live-rate',
+        pathMatch: 'full',
+      },
+      {
+        path: 'about-us',
+        loadComponent: () =>
+          import('./about-us/about-us.component').then(
+            (a) => a.AboutUsComponent
+          ),
+      },
+      {
+        path: 'live-rate',
+        loadComponent: () =>
+          import('./live-rate/live-rate.component').then(
+            (a) => a.LiveRateComponent
+          ),
+      },
+      {
+        path: 'updates',
+        loadComponent: () =>
+          import('./updates/updates.component').then((a) => a.UpdatesComponent),
+      },
+      {
+        path: 'bank-details',
+        loadComponent: () =>
+          import('./bank-details/bank-details.component').then(
+            (a) => a.BankDetailsComponent
+          ),
+      },
+      {
+        path: 'app',
+        loadComponent: () =>
+          import('./apps/apps.component').then(
+            (a) => a.AppsComponent
+          ),
+      },
+      {
+        path: 'eco-calender',
+        loadComponent: () =>
+          import('./eco-calendar/eco-calendar.component').then(
+            (a) => a.EcoCalendarComponent
+          ),
+      },
+      {
+        path: 'contact-us',
+        loadComponent: () =>
+          import('./contact-us/contact-us.component').then(
+            (a) => a.ContactUsComponent
+          ),
+      },
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./login/login.component').then((a) => a.LoginComponent),
+      },
+      {
+        path: '**',
+        redirectTo: 'live-rate',
+        pathMatch: 'full',
+      },
+    ],
+  },
+];
