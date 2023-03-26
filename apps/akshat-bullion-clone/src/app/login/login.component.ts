cee663701805fda7fc93cf01f6a2d3817f40a5ec
Sahil <sahilpatel3584@gmail.com>
2023-03-26 14:32:58 +0530

    login & signup page added

--- apps/akshat-bullion-clone/src/app/login/login.component.ts
+++ apps/akshat-bullion-clone/src/app/login/login.component.ts
@@ -1,14 +1,35 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 
 @Component({
   selector: 'akshat-bull-app-login',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  LoginForm = new FormGroup({
+    UserId: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
+  get UserId() {
+    return this.LoginForm.get('UserId');
+  }
+  get password() {
+    return this.LoginForm.get('password');
+  }
+  Loginuser() {
+    console.warn(this.LoginForm.value);
+  }
+}
+
 
--- apps/akshat-bullion-clone/src/app/sign-up/sign-up.component.ts
+++ apps/akshat-bullion-clone/src/app/sign-up/sign-up.component.ts
@@ -1,22 +1,61 @@
+import { CommonModule, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { FormControl, FormGroup } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'akshat-bull-app-sign-up',
   standalone: true,
-  imports: [CommonModule, RouterModule, ReactiveFormsModule],
+  imports: [CommonModule, NgIf, RouterModule, ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent {
-  name = new FormControl('');
-  profileForm = new FormGroup({
-    tName: new FormControl(''),
-    firmtName: new FormControl(''),
+  SignForm = new FormGroup({
+    Name: new FormControl('', [Validators.required]),
+    FirmName: new FormControl('', [Validators.required]),
+    MobileNumber: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$'),
+    ]),
+    EmailId: new FormControl('', [Validators.required]),
+    City: new FormControl('', [Validators.required]),
+    Password: new FormControl('', [Validators.required]),
+    ConfirmPassword: new FormControl('', [Validators.required]),
+    checkbox: new FormControl('', [Validators.required]),
   });
+  get Name() {
+    return this.SignForm.get('Name');
+  }
+  get FirmName() {
+    return this.SignForm.get('FirmName');
+  }
+  get MobileNumber() {
+    return this.SignForm.get('MobileNumber');
+  }
+  get EmailId() {
+    return this.SignForm.get('EmailId');
+  }
+  get City() {
+    return this.SignForm.get('City');
+  }
+  get Password() {
+    return this.SignForm.get('Password');
+  }
+  get ConfirmPassword() {
+    return this.SignForm.get('ConfirmPassword');
+  }
+  get Signup() {
+    return this.SignForm.get('Signup');
+  }
+  signupuser() {
+    console.warn(this.SignForm.value);
+  }
 }
 
 
@@ -32,11 +71,15 @@ export class SignUpComponent {
 
 
 
+// export const ConfirmPasswordValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+//   const password = control.get('password');
+//   const ConfirmPassword = control.get('confirmPassword');
 
-
+//   return password && ConfirmPassword && password.value === ConfirmPassword.value ? { ConfirmPassword: true } : null;
+// };
 
 //  implements OnInit {
-//   reactiveForm: FormGroup;  
+//   reactiveForm: FormGroup;
 //   ngOnInit(){
 //       this.reactiveForm=new FormGroup({
 //           userid: new FormControl(null),
