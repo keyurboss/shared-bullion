01a899786fba3d02c2b5f53ce26a25d9970bad17
PrathamPatel232 <patelpratham232@gmail.com>
2023-03-27 15:33:24 +0530

    app componant done

--- apps/akshat-gold-clone/src/app/footer/footer.component.ts
+++ apps/akshat-gold-clone/src/app/footer/footer.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'akshat-gold-footer',
+  selector: 'akshat-gold-clone-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+footer_data={
+  aboutus:'About Us ',
+  aboutus_r_l:'about-us',
+  message:'Updates',
+  message_r_l:'updates',
+  bank_d:'Bank Details',
+  bank_d_r_l:'bank-details',
+  contact_us:'Contact Us',
+  contact_us_r_l:'contact-us',
+  copyright_text:'Akshat Gold Pvt Ltd © 2018 I All Rights Reserved',
+};
+}
