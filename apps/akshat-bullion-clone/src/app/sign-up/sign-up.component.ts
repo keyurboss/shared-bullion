a9acebdfc01a86dd9dad0f514cccd110f4657a54
Sahil <sahilpatel3584@gmail.com>
2023-04-01 18:37:40 +0530

    change password minLength

--- apps/akshat-bullion-clone/src/app/sign-up/sign-up.component.ts
+++ apps/akshat-bullion-clone/src/app/sign-up/sign-up.component.ts
@@ -28,7 +28,7 @@ export class SignUpComponent {
     ]),
     EmailId: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),]),
     City: new FormControl('', [Validators.required]),
-    Password: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    Password: new FormControl('', [Validators.required, Validators.minLength(8)]),
     ConfirmPassword: new FormControl('', [Validators.required]),
   })
   get Name() {
