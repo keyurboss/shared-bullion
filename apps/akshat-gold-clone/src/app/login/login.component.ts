90729be635710aa1fe3df4681e35e413510a15ed
PrathamPatel232 <patelpratham232@gmail.com>
2023-04-03 14:54:14 +0530

    all componant tailwind

--- apps/akshat-gold-clone/src/app/login/login.component.ts
+++ apps/akshat-gold-clone/src/app/login/login.component.ts
@@ -8,16 +8,36 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-// import Swal from 'sweetalert2';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'akshat-gold-login',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, RouterOutlet,ReactiveFormsModule,RouterLink],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    RouterOutlet,
+    ReactiveFormsModule,
+    RouterLink,
+  ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  users = [
+    { id: 'sahil@gmail.com', password: 'sahil' },
+    { id: 'Sahil@gmail.com', password: 'sahil' },
+    { id: 'harsh@gmail.com', password: 'harsh' },
+    { id: 'Harsh@gmail.com', password: 'harsh' },
+    { id: 'vraj@gmail.com', password: 'vraj' },
+    { id: 'Vraj@gmail.com', password: 'vraj' },
+    { id: 'bhavya@gmail.com', password: 'bhavya' },
+    { id: 'Bhavya@gmail.com', password: 'bhavya' },
+    { id: 'pratham@gmail.com', password: 'pratham' },
+    { id: 'Pratham@gmail.com', password: 'pratham' },
+    { id: 'alay@gmail.com', password: 'alay' },
+    { id: 'Alay@gmail.com', password: 'alay' },
+  ];
   submitted = false;
   LoginForm = new FormGroup({
     UserId: new FormControl('', [Validators.required]),
@@ -35,17 +55,25 @@ export class LoginComponent {
   get RememberMe() {
     return this.LoginForm.get('RememberMe');
   }
-  // OnLoginuserSubmit() {
-  //   this.submitted = true;
-  //   if (this.UserId?.dirty === false || this.password?.dirty === false) {
-  //     Swal.fire(
-  //       'All details are required!',
-  //       'Please fill out all fields!!',
-  //       'warning'
-  //     );
-  //   } else {
-  //     console.log(this.LoginForm.value);
-  //     Swal.fire('Error', 'User Not Fonund', 'error');
-  //   }
-  // }
+  OnLoginuserSubmit() {
+    this.submitted = true;
+    console.log();
+    if (this.UserId?.dirty === false || this.password?.dirty === false) {
+      Swal.fire('All details are required!', 'Please fill out all fields!!', 'warning');
+    } else {
+      let found = false;
+      for (let i = 0; i < this.users.length; i++) {
+        if (this.users[i].id === this.UserId?.value && this.users[i].password === this.password?.value) {
+          found = true;
+          break;
+        }
+      }
+      if (found) {
+        Swal.fire('', 'Login successful', 'success');
+      } else {
+        Swal.fire('Error', 'User not found', 'error');
+      }
+      console.log(this.LoginForm.value);
+    }
+  }
 }
