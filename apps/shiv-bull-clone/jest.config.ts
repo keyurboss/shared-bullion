caec5f7013f7d3d8951568dcd37e3d08379c09f2
Keyur Shah <keyurshah3939@gmail.com>
2023-04-06 12:28:56 +0530

    Appcomponent TESTS completed

--- apps/shiv-bull-clone/jest.config.ts
+++ apps/shiv-bull-clone/jest.config.ts
@@ -3,15 +3,16 @@ export default {
   displayName: 'shiv-bull-clone',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
-    },
-  },
+  globals: {},
   coverageDirectory: '../../coverage/apps/shiv-bull-clone',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
   },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
