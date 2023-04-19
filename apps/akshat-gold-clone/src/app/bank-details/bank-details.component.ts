9ae9726ccf5f81aa6051560bff714266997740fb
PrathamPatel232 <patelpratham232@gmail.com>
2023-04-19 18:03:59 +0530

    copy to clipboard

--- apps/akshat-gold-clone/src/app/bank-details/bank-details.component.ts
+++ apps/akshat-gold-clone/src/app/bank-details/bank-details.component.ts
@@ -1,36 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ClipboardModule } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'akshat-gold-bank-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ClipboardModule],
   templateUrl: './bank-details.component.html',
   styleUrls: ['./bank-details.component.scss'],
 })
 export class BankDetailsComponent {
+  bank={
+    AN:'921-0200-4224-1964',
+    CAN:'921020042241964',
+    IFSCCODE:'UTIB-0003-606',
+    CIFSCCODE:'UTIB0003606',
+  }
   message = '';
   showMessage = false;
   disabled = false;
-  // clipboard(num: string) {
-  //   navigator.clipboard.writeText(num);
-  // }
   async clipboard(num: any) {
     function hasAlphabet(input: string): boolean {
       const regex = /[a-zA-Z]/;
       return regex.test(input);
     }
     const textToCopy = num;
-    if (navigator.clipboard) {
-      navigator.clipboard.writeText(textToCopy);
-    } else {
-      const textArea = document.createElement('textarea');
-      textArea.value = num;
-      document.body.appendChild(textArea);
-      textArea.select();
-      document.execCommand('copy');
-      document.body.removeChild(textArea);
-    }
     if (!this.disabled) {
       if (hasAlphabet(textToCopy) === true) {
         this.message = 'IFSC Code Copied.....';
