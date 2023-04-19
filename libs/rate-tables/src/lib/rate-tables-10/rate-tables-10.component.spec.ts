3c8768d1229107264cab60807ac4b9822c5d6dac
PrathamPatel232 <patelpratham232@gmail.com>
2023-04-19 18:22:12 +0530

    rate-table-10

---
+++ libs/rate-tables/src/lib/rate-tables-10/rate-tables-10.component.spec.ts
@@ -0,0 +1,32 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RateTables10Component } from './rate-tables-10.component';
+import { 
+  LiveRateService,
+} from '@rps/buillion-frontend-core/services/live-rate.service';
+import { DemoLiveRateService } from '@rps/buillion-frontend-core/mock';
+
+describe('RateTablesComponent', () => {
+  let component: RateTables10Component;
+  let fixture: ComponentFixture<RateTables10Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RateTables10Component],
+      providers: [
+        {
+          provide: LiveRateService,
+          useClass: DemoLiveRateService,
+        },
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(RateTables10Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
