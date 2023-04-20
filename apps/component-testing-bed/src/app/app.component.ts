a2a46ed22ccf7aa088163307be3610a90c68c014
alayAp <alayap510@gmail.com>
2023-04-20 12:02:44 +0530

    create ratetable-8 component

--- apps/component-testing-bed/src/app/app.component.ts
+++ apps/component-testing-bed/src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { DemoLiveRateService } from '@rps/buillion-frontend-core/mock';
 import { LiveRateService } from '@rps/buillion-frontend-core/services';
-import { RateTables6Component, RateTables7Component } from '@rps/bullion-rate-tables';
+import {
+  RateTables6Component,
+  RateTables7Component,
+  RateTables8Component,
+} from '@rps/bullion-rate-tables';
 import { Env } from '@rps/buillion-frontend-core/core';
 @Component({
   standalone: true,
-  imports: [RateTables7Component, RateTables6Component],
+  imports: [RateTables7Component, RateTables8Component, RateTables6Component],
   selector: 'shared-bullion-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
---
+++ libs/rate-tables/src/lib/rate-tables-8/rate-tables-8.component.ts
@@ -0,0 +1,44 @@
+import {JsonPipe, AsyncPipe, NgClass, NgFor, NgIf } from '@angular/common';
+import { Component, Inject, ViewEncapsulation } from '@angular/core';
+import { 
+  LiveRateService,
+  RateObserDataType,
+} from '@rps/buillion-frontend-core/services/live-rate.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'rps-bull-rate-tables-8',
+  standalone: true,
+  imports: [NgClass, NgFor, AsyncPipe, NgIf, JsonPipe],
+  encapsulation: ViewEncapsulation.ShadowDom,
+  templateUrl: './rate-tables-8.component.html',
+  styleUrls: ['./rate-tables-8.component.scss'],
+})
+export class RateTables8Component {
+  rate: Observable<RateObserDataType>;
+  constructor(@Inject(LiveRateService) Pratham: LiveRateService) {
+    this.rate = Pratham.RateObser$.GOLD.asObservable();
+  }
+  rate_class = {
+    red: true,
+    green: false,
+  };
+  data=[
+    {
+      "name": "GOLD 999 IMP WITH TDS",
+      "value":"58860"
+    },
+    {
+      "name": "GOLD 999 IMP WITH TCS",
+      "value":"58915"
+    },
+    {
+      "name": "GOLD 999 LOCAL WITH TDS",
+      "value":"58790"
+    },
+    {
+      "name": "GOLD 999 LOCAL WITH TCS",
+      "value":"58845"
+    },
+  ]
+}
