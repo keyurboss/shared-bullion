8526c4ce6ed2a973bd24c2ebd43a113c7010f2c4
alayAp <alayap510@gmail.com>
2023-04-20 13:45:03 +0530

    table20%completed

--- apps/component-testing-bed/src/app/app.component.ts
+++ apps/component-testing-bed/src/app/app.component.ts
@@ -4,12 +4,15 @@ import { LiveRateService } from '@rps/buillion-frontend-core/services';
 import {
   RateTables6Component,
   RateTables7Component,
+  RateTables2Component,
+  RateTables4Component,
+  RateTables5Component,
   RateTables8Component,
 } from '@rps/bullion-rate-tables';
 import { Env } from '@rps/buillion-frontend-core/core';
 @Component({
   standalone: true,
-  imports: [RateTables7Component, RateTables8Component, RateTables6Component],
+  imports: [RateTables7Component,RateTables2Component,RateTables4Component,RateTables5Component, RateTables8Component, RateTables6Component],
   selector: 'shared-bullion-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
