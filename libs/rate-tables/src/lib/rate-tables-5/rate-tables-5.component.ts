76afea2e38a1dd944be703608a472e9b880a8fd3
Sahil <sahilpatel3584@gmail.com>
2023-04-20 16:22:27 +0530

    responsive table-6

--- libs/rate-tables/src/lib/rate-tables-5/rate-tables-5.component.ts
+++ libs/rate-tables/src/lib/rate-tables-5/rate-tables-5.component.ts
@@ -1,24 +1,11 @@
-import { Component, ViewEncapsulation,Inject,Input } from '@angular/core';
-import { LiveRateService, RateObserDataType } from '@rps/buillion-frontend-core';
-import { Observable } from 'rxjs';
-import { AsyncPipe, JsonPipe, NgFor, NgIf } from '@angular/common';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'rps-bull-rate-tables-5',
   standalone: true,
-  imports: [AsyncPipe, JsonPipe, NgFor, NgIf],
-  encapsulation:ViewEncapsulation.ShadowDom,
+  imports: [],
+  encapsulation: ViewEncapsulation.ShadowDom,
   templateUrl: './rate-tables-5.component.html',
   styleUrls: ['./rate-tables-5.component.scss'],
 })
-export class RateTables5Component {
-  GOLD:Observable<RateObserDataType>;
-  SILVER:Observable<RateObserDataType>;
-  INR:Observable<RateObserDataType>;
-  constructor(@Inject(LiveRateService) sahil:LiveRateService){
-    this.GOLD= sahil.RateObser$.GOLD_SPOT.asObservable();
-    this.SILVER= sahil.RateObser$.SILVER_SPOT.asObservable();
-    this.INR= sahil.RateObser$.INR.asObservable();
-  }
-  @Input() Product_name = 'GOLD SPOT';
-}
+export class RateTables5Component { }
