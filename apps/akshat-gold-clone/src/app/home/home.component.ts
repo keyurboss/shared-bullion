75285e3d665b4f02e0f776b458a10a48abb83d75
PrathamPatel232 <patelpratham232@gmail.com>
2023-04-25 18:14:17 +0530

    menu bar

--- apps/akshat-gold-clone/src/app/home/home.component.ts
+++ apps/akshat-gold-clone/src/app/home/home.component.ts
@@ -3,12 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { popupServices } from '../service/pop-upmenu.service';
 
 @Component({
   selector: 'akshat-gold-home',
   standalone: true,
-  imports: [CommonModule,RouterOutlet,HeaderComponent,FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  menu = false;
+  popup = true;
+  constructor(public _testService: popupServices) {}
+}
---
+++ apps/akshat-gold-clone/src/app/service/pop-upmenu.service.ts
@@ -0,0 +1,10 @@
+import {  Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn:"root"
+})
+export class popupServices{
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    constructor(){}
+     status = false;
+}
