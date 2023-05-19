fe3811190fe775ce7762ef9af453541ff1143884
Keyur Shah <51239095+keyurboss@users.noreply.github.com>
2023-05-19 19:03:48 +0530

    feat(rate-tables): Rate Table 8 (#31)
    Co-authored-by: alayAp <alayap510@gmail.com>

--- apps/component-testing-bed/src/app/app.component.ts
+++ apps/component-testing-bed/src/app/app.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { DemoLiveRateService } from '@rps/buillion-frontend-core/mock';
 import { LiveRateService } from '@rps/buillion-frontend-core/services';
-import { RateTables1Component, RateTables2Component, RateTables3Component, RateTables4Component, RateTables5Component, RateTables6Component, RateTables7Component, RateTables9Component } from '@rps/bullion-rate-tables';
+import {
+  RateTables6Component,
+  RateTables7Component,
+  RateTables2Component,
+  RateTables4Component,
+  RateTables5Component,
+  RateTables8Component,
+} from '@rps/bullion-rate-tables';
 import { Env } from '@rps/buillion-frontend-core/core';
 @Component({
   standalone: true,
-  imports: [RateTables1Component, RateTables2Component, RateTables3Component, RateTables4Component, RateTables5Component, RateTables7Component, RateTables6Component, RateTables9Component],
+  imports: [
+    RateTables7Component,
+    RateTables2Component,
+    RateTables4Component,
+    RateTables5Component,
+    RateTables8Component,
+    RateTables6Component,
+  ],
   selector: 'shared-bullion-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
