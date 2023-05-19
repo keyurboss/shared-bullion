0a3d4c40012aeaf772bfa87f527fcd108457bd84
Keyur Shah <keyurshah3939@gmail.com>
2023-05-19 17:52:54 +0530

    Completd

--- libs/rate-tables/src/lib/rate-tables-9/rate-tables-9.component.spec.ts
+++ libs/rate-tables/src/lib/rate-tables-9/rate-tables-9.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RateTables9Component } from './rate-tables-9.component';
+import { LiveRateService } from '@rps/buillion-frontend-core';
+import { DemoLiveRateService } from '@rps/buillion-frontend-core/mock';
 
 describe('RateTablesComponent', () => {
   let component: RateTables9Component;
@@ -9,6 +11,12 @@ describe('RateTablesComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RateTables9Component],
+      providers: [
+        {
+          provide: LiveRateService,
+          useClass: DemoLiveRateService,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(RateTables9Component);
