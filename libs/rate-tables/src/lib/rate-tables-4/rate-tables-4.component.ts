7a6d45dbbcfd2b1ff0241080aa15b6f50c863ed8
Sahil <sahilpatel3584@gmail.com>
2023-05-22 12:58:32 +0530

    Responsive

--- libs/rate-tables/src/lib/rate-tables-4/rate-tables-4.component.ts
+++ libs/rate-tables/src/lib/rate-tables-4/rate-tables-4.component.ts
@@ -4,12 +4,12 @@ import {
   RateObserDataType,
 } from '@rps/buillion-frontend-core';
 import { Observable } from 'rxjs';
-import { AsyncPipe, JsonPipe, NgFor, NgIf } from '@angular/common';
+import { AsyncPipe, CommonModule, JsonPipe, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'rps-bull-rate-tables-4',
   standalone: true,
-  imports: [AsyncPipe, JsonPipe, NgFor, NgIf],
+  imports: [AsyncPipe, CommonModule, JsonPipe, NgFor, NgIf],
   encapsulation: ViewEncapsulation.ShadowDom,
   templateUrl: './rate-tables-4.component.html',
   styleUrls: ['./rate-tables-4.component.scss'],
@@ -19,7 +19,7 @@ export class RateTables4Component {
   SILVER: Observable<RateObserDataType>;
 
   constructor(@Inject(LiveRateService) pr: LiveRateService) {
-    this.GOLD = pr.RateObser$.GOLD_SPOT.asObservable();
-    this.SILVER = pr.RateObser$.SILVER_SPOT.asObservable();
+    this.GOLD = pr.RateObser$.GOLD.asObservable();
+    this.SILVER = pr.RateObser$.SILVER.asObservable();
   }
 }
