eee67a3c6758b198357aa9fa9be4c369a1a6b3ae
Sahil <sahilpatel3584@gmail.com>
2023-05-30 14:45:57 +0530

    table 3 test case

--- libs/rate-tables/src/lib/rate-tables-3/rate-tables-3.component.ts
+++ libs/rate-tables/src/lib/rate-tables-3/rate-tables-3.component.ts
@@ -5,6 +5,13 @@ import {
   LiveRateService,
   RateObserDataType,
 } from '@rps/buillion-frontend-core';
+import { RateBaseSymboles } from '@rps/bullion-interfaces';
+export interface data {
+  symbole: RateBaseSymboles
+  Header: string;
+  sell: string;
+  productname: { name: string }[]
+}
 
 @Component({
   selector: 'rps-bull-rate-tables-3',
@@ -15,26 +22,18 @@ import {
   styleUrls: ['./rate-tables-3.component.scss'],
 })
 export class RateTables3Component {
-  rate: Observable<RateObserDataType>;
-  @Input() productName = 'GOLD PRODUCTS';
-
-  @Input() sell = 'SELL';
-  data = [
-    {
-      name: 'GOLD 999 IMP WITH TDS',
-    },
-    {
-      name: 'GOLD 999 IMP WITH TCS',
-    },
-    {
-      name: 'GOLD 999 LOCAL WITH TDS',
-    },
-    {
-      name: 'GOLD 999 LOCAL WITH TCS',
-    },
-  ];
-
-  constructor(@Inject(LiveRateService) sahil: LiveRateService) {
-    this.rate = sahil.RateObser$.GOLD.asObservable();
+  @Input()
+  private _table: data[] = [];
+  public get table(): data[] {
+    return this._table;
+  }
+  public set table(value: data[]) {
+    value.forEach(({ symbole }) => {
+      this.RateObser$[symbole] = this.rateObservar.RateObser$[symbole].asObservable()
+    })
+    this._table = value;
+  }
+  RateObser$: Record<RateBaseSymboles, Observable<RateObserDataType>> = {} as never
+  constructor(@Inject(LiveRateService) private readonly rateObservar: LiveRateService) {
   }
 }
