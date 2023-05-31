07c05f9de283130646152ec1f1c87697327dac83
Sahil <sahilpatel3584@gmail.com>
2023-05-31 14:15:56 +0530

    table 6 add table length test case

--- libs/rate-tables/src/lib/rate-tables-6/rate-tables-6.component.spec.ts
+++ libs/rate-tables/src/lib/rate-tables-6/rate-tables-6.component.spec.ts
@@ -1,12 +1,14 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
+import { faker } from '@faker-js/faker';
 import { RateTables6Component } from './rate-tables-6.component';
 import { LiveRateService } from '@rps/buillion-frontend-core';
-import { DemoLiveRateService } from '@rps/buillion-frontend-core/mock';
+import { DemoLiveRateService, InitialiseRemoteConnection } from '@rps/buillion-frontend-core/mock';
+import { RateBaseSymboles } from '@rps/bullion-interfaces';
 
 describe('RateTablesComponent', () => {
   let component: RateTables6Component;
   let fixture: ComponentFixture<RateTables6Component>;
+  let componentHtml: ShadowRoot;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RateTables6Component],
@@ -15,15 +17,43 @@ describe('RateTablesComponent', () => {
           provide: LiveRateService,
           useClass: DemoLiveRateService,
         },
+        {
+          provide: InitialiseRemoteConnection,
+          useValue: false,
+        }
       ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(RateTables6Component);
     component = fixture.componentInstance;
+    componentHtml = (fixture.nativeElement as HTMLElement).shadowRoot!;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  describe('Rate Table 3 1st TestCase For Name', () => {
+    test('check Header all Text && products length and Name', fakeAsync(() => {
+      component.headers = {
+        HeaderName: 'FUTURE RATE',
+        bid: 'BID',
+        diff: 'DIFF',
+        ask: 'ASK',
+      }
+      component.table = [
+        {
+          ProductName: 'GOLD FUTURE',
+          symbole: RateBaseSymboles.GOLD_MCX,
+        },
+        {
+          ProductName: 'SILVER FUTURE',
+          symbole: RateBaseSymboles.SILVER_MCX,
+        },
+      ];
+      fixture.detectChanges();
+      const footerlength = componentHtml.querySelectorAll('.footer_item')?.length;
+      expect(footerlength).toStrictEqual(component.table.length)
+    }))
+  })
 });
