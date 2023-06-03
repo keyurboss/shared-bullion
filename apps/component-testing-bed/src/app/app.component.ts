0000001cf939fac82c2002fbd02de6f49b3d51f8
sahil patel <106461848+sahiiilpatel@users.noreply.github.com>
2023-06-03 18:39:25 +0530

    fix(rate-tables): table 9 (#36)
    Co-authored-by: Keyur Shah <keyurshah3939@gmail.com>

--- apps/component-testing-bed/src/app/app.component.ts
+++ apps/component-testing-bed/src/app/app.component.ts
@@ -3,12 +3,10 @@ import { Component } from '@angular/core';
 import { Env } from '@rps/buillion-frontend-core/core';
 import { DemoLiveRateService } from '@rps/buillion-frontend-core/mock';
 import { LiveRateService } from '@rps/buillion-frontend-core/services';
-import { RateTables6Component } from '@rps/bullion-rate-tables/table6';
-
-import { RateTables10Component, RateTables7Component } from '@rps/bullion-rate-tables';
+import { RateTables1Component, RateTables2Component, RateTables3Component, RateTables4Component, RateTables5Component, RateTables6Component, RateTables7Component, RateTables9Component } from '@rps/bullion-rate-tables';
 @Component({
   standalone: true,
-  imports: [RateTables7Component, RateTables6Component, RateTables10Component],
+  imports: [RateTables1Component, RateTables2Component, RateTables3Component, RateTables4Component, RateTables5Component, RateTables7Component, RateTables6Component, RateTables9Component],
   selector: 'shared-bullion-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
