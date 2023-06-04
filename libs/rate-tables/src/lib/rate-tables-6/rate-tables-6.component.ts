fe87a2dab983a1ec709861e81d110de9d906790f
sahil patel <106461848+sahiiilpatel@users.noreply.github.com>
2023-06-04 12:24:54 +0530

    feat(rate-tables): rate tables 6 tests (#42)

--- libs/rate-tables/src/lib/rate-tables-6/rate-tables-6.component.ts
+++ libs/rate-tables/src/lib/rate-tables-6/rate-tables-6.component.ts
@@ -1,31 +1,43 @@
-import { AsyncPipe, JsonPipe, NgIf } from '@angular/common';
-import { Component, Inject, ViewEncapsulation } from '@angular/core';
+import { AsyncPipe, JsonPipe, NgIf, NgFor } from '@angular/common';
+import { Component, Inject, Input, ViewEncapsulation } from '@angular/core';
 import {
   LiveRateService,
   RateObserDataType,
 } from '@rps/buillion-frontend-core';
+import { RateBaseSymboles } from '@rps/bullion-interfaces';
 import { Observable } from 'rxjs';
+export interface data {
+  symbole: RateBaseSymboles
+  ProductName: string
+}
 
 @Component({
   selector: 'rps-bull-rate-tables-6',
   standalone: true,
-  imports: [AsyncPipe, JsonPipe, NgIf],
+  imports: [AsyncPipe, JsonPipe, NgIf, NgFor],
   encapsulation: ViewEncapsulation.ShadowDom,
   templateUrl: './rate-tables-6.component.html',
   styleUrls: ['./rate-tables-6.component.scss'],
 })
 export class RateTables6Component {
-  headers = {
-    productName: 'FUTURE RATE',
-    bid: 'BID',
-    diff: 'DIFF',
-    ask: 'ASK',
+  @Input() headers = {
+    HeaderName: '',
+    bid: '',
+    diff: '',
+    ask: '',
   };
-
-  GOLD: Observable<RateObserDataType>;
-  SILVER: Observable<RateObserDataType>;
-  constructor(@Inject(LiveRateService) sahil: LiveRateService) {
-    this.GOLD = sahil.RateObser$.GOLD_MCX.asObservable();
-    this.SILVER = sahil.RateObser$.SILVER_MCX.asObservable();
+  @Input()
+  private _table: data[] = [];
+  public get table(): data[] {
+    return this._table;
+  }
+  public set table(value: data[]) {
+    value.forEach(({ symbole }) => {
+      this.RateObser$[symbole] = this.rateObservar.RateObser$[symbole].asObservable()
+    })
+    this._table = value;
+  }
+  RateObser$: Record<RateBaseSymboles, Observable<RateObserDataType>> = {} as never
+  constructor(@Inject(LiveRateService) private readonly rateObservar: LiveRateService) {
   }
 }
