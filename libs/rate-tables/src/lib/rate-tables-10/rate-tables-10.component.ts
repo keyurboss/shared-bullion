1f569f80528aa3768ece0f1c470721d3b93a06f5
alayAp <alayap510@gmail.com>
2023-06-14 17:21:23 +0530

    table 10 html and ts done

--- libs/rate-tables/src/lib/rate-tables-10/rate-tables-10.component.ts
+++ libs/rate-tables/src/lib/rate-tables-10/rate-tables-10.component.ts
@@ -1,6 +1,11 @@
 import { AsyncPipe, JsonPipe, NgClass, NgFor, NgIf } from '@angular/common';
 import { Component, ViewEncapsulation } from '@angular/core';
-
+interface table10{
+  name:string;
+  rate:string;
+  imageLink:string;
+  backgroundColor?:string;
+}
 @Component({
   selector: 'rps-bull-rate-tables-10',
   standalone: true,
@@ -9,4 +14,22 @@ import { Component, ViewEncapsulation } from '@angular/core';
   templateUrl: './rate-tables-10.component.html',
   styleUrls: ['./rate-tables-10.component.scss'],
 })
-export class RateTables10Component {}
+export class RateTables10Component {
+  data :table10[]=[
+    {
+      name:"GOLD",
+      imageLink:"https://akshatgold.com/assets/img/gold_bar_back.png",
+      rate:"1997.50"
+    },
+    {
+      name:"SILVER",
+      imageLink:"https://akshatgold.com/assets/img/silver_bar_back.png",
+      rate:"25.40"
+    },
+    {
+      name:"INR",
+      imageLink:"https://akshatgold.com/assets/img/rabi_logo.png",
+      rate:"82.1600"
+    }
+  ]
+}
