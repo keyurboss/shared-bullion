17f66e807b55654da8418661f45d5296b112764c
Keyur Shah <51239095+keyurboss@users.noreply.github.com>
2023-07-18 06:31:57 -0700

    feat(rate-table): rate table 10 tests (#52)
    Co-authored-by: alayAp <alayap510@gmail.com>

--- libs/rate-tables/src/lib/rate-tables-10/rate-tables-10.component.spec.ts
+++ libs/rate-tables/src/lib/rate-tables-10/rate-tables-10.component.spec.ts
@@ -1,14 +1,14 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 
-import { RateTables10Component } from './rate-tables-10.component';
-import { 
-  LiveRateService,
-} from '@rps/buillion-frontend-core';
+import { LiveRateService } from '@rps/buillion-frontend-core';
 import { DemoLiveRateService } from '@rps/buillion-frontend-core/mock';
+import { RateTables10Component } from './rate-tables-10.component';
+export const InitialiseRemoteConnection = 'initialiseRemoteConnection';
 
 describe('RateTablesComponent', () => {
   let component: RateTables10Component;
   let fixture: ComponentFixture<RateTables10Component>;
+  let componentHtml: ShadowRoot;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,15 +18,57 @@ describe('RateTablesComponent', () => {
           provide: LiveRateService,
           useClass: DemoLiveRateService,
         },
+        {
+          provide: InitialiseRemoteConnection,
+          useValue: false,
+        },
       ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(RateTables10Component);
     component = fixture.componentInstance;
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    componentHtml = (fixture.nativeElement as HTMLElement).shadowRoot!;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Rate Table 10 1st TestCase For values', () => {
+    test('check all products Name', fakeAsync(() => {
+      component.data = [
+        {
+          name: 'GOLD',
+          imageLink: 'https://akshatgold.com/assets/img/gold_bar_back.png',
+          rate: '1997.50',
+        },
+        {
+          name: 'SILVER',
+          imageLink: 'https://akshatgold.com/assets/img/silver_bar_back.png',
+          rate: '25.40',
+        },
+        {
+          name: 'INR',
+          imageLink: 'https://akshatgold.com/assets/img/rabi_logo.png',
+          rate: '82.1600',
+        },
+      ];
+      fixture.detectChanges();
+      for (let i = 0; i < 3; i++) {
+        const productsname = componentHtml
+          .querySelectorAll('.name')
+          [i]?.textContent?.trim();
+        expect(productsname).toStrictEqual(component.data[i]?.name);
+        const productsrate = componentHtml
+          .querySelectorAll('.rate')
+          [i]?.textContent?.trim();
+        expect(productsrate).toStrictEqual(component.data[i]?.rate);
+        // const shadow = fixture.debugElement.nativeElement.shadowRoot;
+        const productsimg = componentHtml.querySelectorAll('img');
+        expect(productsimg[i]?.src).toStrictEqual(component.data[i]?.imageLink);
+      }
+    }));
   });
 });
--- libs/rate-tables/src/lib/rate-tables-10/rate-tables-10.component.ts
+++ libs/rate-tables/src/lib/rate-tables-10/rate-tables-10.component.ts
@@ -1,6 +1,11 @@
 import { AsyncPipe, JsonPipe, NgClass, NgFor, NgIf } from '@angular/common';
 import { Component, ViewEncapsulation } from '@angular/core';
-
+interface table10 {
+  name: string;
+  rate: string;
+  imageLink: string;
+  backgroundColor?: string;
+}
 @Component({
   selector: 'rps-bull-rate-tables-10',
   standalone: true,
@@ -9,4 +14,6 @@ import { Component, ViewEncapsulation } from '@angular/core';
   templateUrl: './rate-tables-10.component.html',
   styleUrls: ['./rate-tables-10.component.scss'],
 })
-export class RateTables10Component {}
+export class RateTables10Component {
+  data: table10[] = [];
+}
