8096fa832b40cd95657795ce170c48dfe6a676d0
Keyur Shah <keyurshah3939@gmail.com>
2023-11-10 02:26:08 +0530

    minor

--- apps/server/whatsapp-common/src/firebaseRD.creds.store.ts
+++ apps/server/whatsapp-common/src/firebaseRD.creds.store.ts
@@ -9,24 +9,28 @@ import {
   proto,
 } from '@whiskeysockets/baileys';
 import { firebaseDb } from './firebase.app';
+function processFileName(name: string): string {
+  return name.replaceAll('.', '@');
+}
 export const useFireBaseRealTimeDatabaseStoreAuthState = async (
   collectionName: string,
   serverName: string,
 ): Promise<{ state: AuthenticationState; saveCreds: () => Promise<void> }> => {
   const collection = firebaseDb.ref(collectionName).child(serverName);
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+
   const writeData = async (data: any, file: string) => {
     data = JSON.stringify(data, BufferJSON.replacer);
     console.log('Writing data type ', typeof data);
     console.log('Writing data  ', data);
-    collection.child(file).set(data);
+    collection.child(processFileName(file)).set(data);
     return;
   };
 
   const readData = async (file: string) => {
     try {
       console.log('Reading ', file);
-      const data = await collection.child(file).get();
+      const data = await collection.child(processFileName(file)).get();
       if (!data.exists()) {
         return null;
       }
